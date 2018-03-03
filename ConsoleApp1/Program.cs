c960617776ef4b9df17ee8115a08f492bc442513
02484220 <02484220@02484220-N2>
2018-03-03 18:43:21 +0300

    ICalculator eklendi.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿using Calculations;
+using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var c = new Calculator();
+            Console.WriteLine(c.Add(3, 4));
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,20 @@
+﻿using Calculations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var c = new Calculator();
+            Console.WriteLine(c.Add(3, 4));
+
+            Console.ReadLine();
+        }
+    }
+}
